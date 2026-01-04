1bab566c08cc38518766e4ecab269fade5142e9f
LautaroLobo12 <lautarol9@outlook.com>
2026-01-04 14:38:45 +0000

    New Prod Build 4

--- _astro/index.astro_astro_type_script_index_0_lang.DufJrYPi.js
+++
@@ -1,1 +0,0 @@
-import{w as o}from"./auth.BI9pXLoy.js";import"./firebase.DTFJdJwj.js";import"./firestore.ClxTndq5.js";o().then(i=>{i&&(window.location.href="/picks")});
---
+++ _astro/index.astro_astro_type_script_index_0_lang.Tl2ZHfaZ.js
@@ -0,0 +1,1 @@
+import{w as o}from"./auth.BtpwsKjq.js";import"./firebase.DTFJdJwj.js";import"./firestore.BwIhdLTQ.js";o().then(i=>{i&&(window.location.href="/picks")});
